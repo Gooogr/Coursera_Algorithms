cd7e91ab8638070d93327954cce3b31c815633df
Gooogr <gusarovga@ya.ru>
2020-03-19 01:24:19 +0300

    Solve programmming assignment for week 2. Create folder with assignment data.

---
+++ w2_CountingInversions(programming_assignment).py
@@ -0,0 +1,58 @@
+def countInv_simple(arr):
+	'''
+	Straightforward appproach to count amount of inversions in a array.
+	O = n**2
+	'''
+	i, j = 0, 0
+	numInv = 0
+	for i in range(len(arr)):
+		for j in range(i, len(arr) - 1):
+			if arr[i] > arr[j]:
+				numInv += 1
+	return numInv
+
+
+# Let`s write something better	
+totalInv = 0
+
+def SortAndCountInv(arr):
+	if len(arr) < 2:
+		return (arr, 0)
+	else:
+		n = len(arr)//2
+		#print('Splited arrays: ', n, arr[:n], arr[n:])
+		(left_arr, left_inv) = SortAndCountInv(arr[:n])
+		(right_arr, right_inv) = SortAndCountInv(arr[n:])
+		(merged_arr, split_inv) = MergeAndCountInv(left_arr, right_arr)
+		return (merged_arr, left_inv + right_inv + split_inv)
+		
+def MergeAndCountInv(left_arr, right_arr):
+	global totalInv
+	result = []
+	i, j = 0, 0
+	splitInv = 0
+	#print()
+	#print('Merging arrays: ', left_arr, right_arr)
+	while len(left_arr) != 0 and len(right_arr) != 0:
+		if left_arr[0] <= right_arr[0]:
+			result.append(left_arr[0])
+			left_arr.remove(left_arr[0])
+		else:
+			result.append(right_arr[0])
+			right_arr.remove(right_arr[0])
+			splitInv += len(left_arr)
+	if len(left_arr) == 0:
+		result += right_arr
+	else:
+		result += left_arr
+	#print('Result: ', result)
+	#print()
+	return (result, splitInv)
+			
+file_path = '/home/grigoriy/Desktop/Repos/Coursera_Algorithms/WeeklyTasksData/IntegerArray.txt'
+with open(file_path, 'r') as f:
+	numList = [int(integers.strip()) for integers in f.readlines()]
+	f.close()
+
+# ~ print(countInv_simple(arr_1))
+print(SortAndCountInv(numList)[1])
